e5ee321345f5ceef59275257c1a7c06332dbf837
milee8989 <milee8989@gmail.com>
2019-06-16 00:14:28 -0500

    javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+
+var correct = 0;
+var incorrect = 0;
+var counter = 60; //60seconds
+
+// array of questions
+
+var questions = [
+    {q: "color of banana?", a: ["blue","yellow","red","black"], correctAnswer: "yellow"},
+    {q: "color of banana?", a: ["blue","yellow","red","black"], correctAnswer: "yellow"},
+    {q: "color of banana?", a: ["blue","yellow","red","black"], correctAnswer: "yellow"},
+    {q: "color of banana?", a: ["blue","yellow","red","black"], correctAnswer: "yellow"},
+];
+
+//functions
+//start game
+//countdown
+//checkResults
+//showResults
